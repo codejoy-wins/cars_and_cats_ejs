b2ecf84c1f600d4eb4f005439cb6837f0b069fad
Max <mjann@ucsc.edu>
2018-05-10 15:56:47 -0700

    overlap

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+app.set('views', __dirname + '/views');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + "/static"));
+
+app.get('/cars', function(request, response){
+
+    response.render('cars');
+})
+
+app.get('/cats', function(request, response){
+
+    response.render('cats');
+})
+
+app.get('/cars/new', function(request, response){
+
+    response.render('newcar');
+})
+
+app.listen(8000, function(){
+    console.log("Listening on port 8000");
+})
